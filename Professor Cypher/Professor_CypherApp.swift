d67911da54f1ec1fe3daefd4ab6bd2d5fe419eff
Jack Dellamea <jdellamea@lcs.on.ca>
2023-06-05 09:05:37 -0400

    Initial Commit

---
+++ Professor Cypher/Professor_CypherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Professor_CypherApp.swift
+//  Professor Cypher
+//
+//  Created by Jack Dellamea on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Professor_CypherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
