5e51dcb75d3b12d4689e72487609e0e6e2c213c5
Jack Dellamea <jdellamea@lcs.on.ca>
2023-06-05 19:44:13 -0400

    Started creating the view for the Rail Fence Cipher

---
+++ Professor Cypher/RailFenceCipherView.swift
@@ -0,0 +1,24 @@
+//
+//  RailFenceCipherView.swift
+//  Professor Cypher
+//
+//  Created by Jack Dellamea on 6/5/23.
+//
+
+import SwiftUI
+
+struct RailFenceCipherView: View {
+    var body: some View {
+        VStack{
+            Text("Rail Fence Cipher")
+                .font(.system(size: 30))
+            Divider()
+           Spacer()
+        }
+    }
+}
+struct RailFenceCipherView_Previews: PreviewProvider {
+    static var previews: some View {
+        RailFenceCipherView()
+    }
+}
