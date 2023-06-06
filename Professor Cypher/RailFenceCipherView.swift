e2114faca2a26fc43179b18690050f28838b6a0c
Jack Dellamea <jdellamea@lcs.on.ca>
2023-06-05 20:00:46 -0400

    Text and picture and unusable "back" button added for the Rail Fence Cipher.

--- Professor Cypher/RailFenceCipherView.swift
+++ Professor Cypher/RailFenceCipherView.swift
@@ -13,7 +13,22 @@ struct RailFenceCipherView: View {
             Text("Rail Fence Cipher")
                 .font(.system(size: 30))
             Divider()
-           Spacer()
+            
+            Image("RailFenceCipher")
+                .resizable()
+                .scaledToFit()
+            Divider()
+            VStack{
+                HStack{
+                    Text("The Rail Fence Cipher was created by the Greeks. They also created special tool called a scytale that helped them to decode their messages to the spartans during military campagns. Now a days it is usually written on regular paper and decoded without any assistance necissary. ")
+                        .font(.system(size: 18))
+                }
+            Divider()
+            }
+            Spacer()
+            Divider()
+            Button("Back") {print("back")}
+                .font(.system(size:20))
         }
     }
 }
